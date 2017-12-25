271b4e584a7767e3c341c320e3d95cac0a67668a
huyhieuno1 <nguyenhuuhieu123321@gmail.com>
2017-12-25 16:47:05 +0700

    Add files via upload

---
+++ WebDocTruyen/src/main/java/group1/controller/LoginController.java
@@ -0,0 +1,21 @@
+package group1.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping(value = "/login", method=RequestMethod.GET)
+	public ModelAndView displayLogin(HttpServletRequest arg0, HttpServletResponse arg1){
+		
+		return new ModelAndView("login");
+	}
+	
+	
+}
